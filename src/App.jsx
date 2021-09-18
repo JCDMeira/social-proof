61c23cc09b749351169df967488bd8794e21792d
Jean Meira <jean.meira10@hotmail.com>
2021-09-18 15:11:14 -0300

    Add: app with space division

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import "./App.css";
+import React from "react";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="conteiner">
+        <div className="content">
+          <div className="topInfo">
+            <div className="infoText">
+              <h1>10,000+ of our users love our products.</h1>
+              <p>
+                We only provide great products combined with excellent customer
+                service. See what our satisfied customers are saying about our
+                services.
+              </p>
+            </div>
+            <div className="infoStar"></div>
+          </div>
+
+          <div className="bottomInfo"></div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
